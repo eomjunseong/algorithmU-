97340f531d650aa0b44f9779f036fe05713024a2
eomjunseung <hi_man159@naver.com>
2022-05-09 17:58:07 +0900

    '0509-01'

--- src/example01/ElectricityBill_V1.java
+++ src/example01/ElectricityBill_V1.java
@@ -10,7 +10,7 @@ public class ElectricityBill_V1 {
 
         Scanner scanner = new Scanner(System.in);
         double answer = scanner.nextDouble();
-        System.out.println(AddFunc(answer));
+        System.out.println(AddFunc(answer)); //계산 출력
     }
     public static double AddFunc(double a){
 //            저압의 경우
@@ -21,22 +21,16 @@ public class ElectricityBill_V1 {
 //            400kWh 초과는 417.7원,
 //            500kWh초과는 670.6원의
 //            전력량 요금을 내야한다.
-        double sum = 0 ; // 초기값
-        double target = a;
-        if(target<=100){
-            sum=target*60.7;
-            return sum;
-        }else{
-           sum+=100*60.7;
-        }
-        target-=100;
-
-        if(target<=100){
-            sum=target*60.7;
-            return sum;
-        }else{
-            sum+=100*60.7;
-        }
-        return sum;
+        if(a>500) {
+            return (100*60.7)+(100*125.9)+(100*187.9)+(100*280.6)+(100*417.7)+((a-500)*670.6);
+        }else if(a>400){
+            return (100*60.7)+(100*125.9)+(100*187.9)+(100*280.6)+((a-400)*417.7);
+        }else if(a>300){
+            return (100*60.7)+(100*125.9)+(100*187.9)+((a-300)*280.6);
+        }else if(a>200){
+            return (100*60.7)+(100*125.9)+((a-200)*187.9);
+        }else if(a>100){
+            return (100*60.7)+((a-100)*125.9);
+        }else return a*60.7;
     }
 }
