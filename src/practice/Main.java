bb2804519c9017e866892a594261b228d2f81da7
eomjunseung <hi_man159@naver.com>
2022-05-16 17:37:47 +0900

    0516-5

---
+++ src/practice/Main.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Main {
+    public static void main(String[] args){
+
+
+             String requestDate = "2018-03-02";
+
+           requestDate.replace("-", "");
+
+
+         System.out.println(requestDate);
+    }
+}
