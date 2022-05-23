c0edfa0f1bd25ed7b504539656ecc6c0d6ad3ed2
eomjunseong <hi_man159@naver.com>
2022-05-23 13:58:06 +0900

    0523-004

--- src/practice/Test.java
+++ src/practice/Test.java
@@ -1,11 +1,17 @@
 package practice;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.IntStream;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-class MatchStream {
-    public static void main(String[] args) {
-
+class Solution {
+    public List<String> mergeTwoStream(List<String> list1, List<String> list2) {
+        Stream<String> s1 = list1.stream();
+        Stream<String> s2 = list2.stream();
+        return Stream.concat(s1,s2).collect(Collectors.toList());
     }
 }
